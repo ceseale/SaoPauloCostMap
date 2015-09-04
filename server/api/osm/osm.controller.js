69685b87a8fc83af2854730bddd9f255fefd3897
Colin Seale <ceseale3@gmail.com>
2015-09-04 11:09:04 -0400

    setup grid calculations and removed isochrone usage

--- server/api/osm/osm.controller.js
+++ server/api/osm/osm.controller.js
@@ -5,30 +5,26 @@
 
 'use strict';
 var isochrone = require('osrm-isochrone');
-// Gets a list of Osms
-
- 
-
-
+// Gets a list of Osms 
+   
 exports.index = function(req, res) {
-
 var resolution = 25; // sample resolution
 var network = "null" ; 
 var time = 3600; // 300 second drivetime (5 minutes)
 var location = [-46.648171 ,-23.539278 ]; // center point
 var out = [];
-var cal = [300,600,900,1200,1500];
+var cal = [300,900,900,1200,1500];
 
-cal.forEach(function (item , i ){
-	if(i == 0){
-	isochrone(location, cal[i], resolution,  network  , function(err, drivetime) {
-  if(err) throw err;
-  out.push((drivetime))
-});
-}
-})
-
-
+// cal.forEach(function (item , i ){
+// 	if(i == 0){
+// 	isochrone(location, cal[i], resolution,  network  , function(err, drivetime) {
+//   if(err) throw err;
+//   out.push((drivetime))
+// });
+// }
+// })   
+       
+ 
 isochrone(location, cal[1], resolution, network , function(err, drivetime) {
   if(err) throw err;
   out.push((drivetime))
@@ -41,7 +37,7 @@ isochrone(location, cal[1], resolution, network , function(err, drivetime) {
 
 
   // var EdmundsClient = require('node-edmunds-api');
-  // var client = new EdmundsClient({ apiKey: 'API-KEY' });
+  // var client = new EdmundsClient({ apiKey: 'hamkxagh9shf9twh6tq6xxy4' });
   // console.log(client.getAllMakes({}, function (err, res){
   // 	console.log(res)
   // }))
